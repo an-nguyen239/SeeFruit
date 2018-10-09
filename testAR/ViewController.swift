1a4656e927ba839ce70200b5b9c251d958e457d4
An Nguyen <anguyen108@student.gsu.edu>
2018-10-08 23:36:25 -0400

    setting up AR project

---
+++ testAR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testAR
+//
+//  Created by An Nguyen on 10/6/18.
+//  Copyright © 2018 An Nguyen. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+    
+
+}
+
+
+

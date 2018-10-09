c534068934bfe7aef5ac08e3fc6a66755476b253
An Nguyen <anguyen108@student.gsu.edu>
2018-10-09 01:27:30 -0400

    detect surface to add AR plane

--- testAR/ViewController.swift
+++ testAR/ViewController.swift
@@ -44,6 +44,49 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
+    
+    
+    func getFruitType() -> String {
+        
+        return "tomato"
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        let fruitType = getFruitType()
+        let string1 = "-pop.png"
+        let image_path = fruitType + string1
+        if anchor is ARPlaneAnchor {
+            
+            print("plane detected")
+            
+            let planeAnchor = anchor as! ARPlaneAnchor
+            
+            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+            
+            
+            let planeNode = SCNNode()
+            
+            planeNode.position = SCNVector3(planeAnchor.center.x, 0.3, planeAnchor.center.z)
+            
+            
+            let gridMaterial = SCNMaterial()
+            
+            gridMaterial.diffuse.contents = UIImage(named: image_path)
+            gridMaterial.isDoubleSided = true
+            plane.materials = [gridMaterial]
+            
+            
+            
+            planeNode.geometry = plane
+            
+            
+            
+            node.addChildNode(planeNode)
+            
+        } else {
+            return
+        }
+    }
 }
 
     
